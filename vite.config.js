8e4505098323aa3a34e53a2f186d9da985e0c11a
Suhag M P <5uhag@proton.me>
2026-02-27 07:27:28 +0530

    fix: set relative base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+    base: './', // Ensures assets are loaded relative to the index.html on GitHub pages
     plugins: [
         basicSsl()
     ]
